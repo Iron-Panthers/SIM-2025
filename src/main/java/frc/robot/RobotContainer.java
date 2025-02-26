6ec691c23aefd98fd1e8e6c302ab8e6de974ee05
audrey <audreypjohnson@gmail.com>
2025-02-25 17:22:41 -0800

    path for testing, add intake and L4 as registered commands

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -158,11 +158,13 @@ public class RobotContainer {
     superstructure = new Superstructure(elevator, pivot, tongue);
 
     // Register Command Names
-    NamedCommands.registerCommand("Intake", 
-            new SequentialCommandGroup(
-                superstructure.goToStateCommand(SuperstructureState.INTAKE),
-                rollers.setTargetCommand(RollerState.FORCE_INTAKE)));
-    NamedCommands.registerCommand("Score_L4", superstructure.goToStateCommand(SuperstructureState.SCORE_L4));
+    NamedCommands.registerCommand(
+        "Intake",
+        new SequentialCommandGroup(
+            superstructure.goToStateCommand(SuperstructureState.INTAKE),
+            rollers.setTargetCommand(RollerState.FORCE_INTAKE)));
+    NamedCommands.registerCommand(
+        "Score_L4", superstructure.goToStateCommand(SuperstructureState.SCORE_L4));
 
     configureBindings();
     configureAutos();
