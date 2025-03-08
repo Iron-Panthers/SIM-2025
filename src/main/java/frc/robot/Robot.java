ef1aec6c5c48594f7e508e875722e18d441d242a
Jay <jaykang445@gmail.com>
2025-03-08 18:23:06 +0000

    update dashboard with relevant starting statuses

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -116,7 +116,9 @@ public class Robot extends LoggedRobot {
 
   /** This function is called periodically when disabled. */
   @Override
-  public void disabledPeriodic() {}
+  public void disabledPeriodic() {
+    robotContainer.updateDashboardStatus();
+  }
 
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
