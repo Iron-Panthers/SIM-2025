f0e2651556484f5957bf438ac376450b8bf4a48f
audrey <audreypjohnson@gmail.com>
2025-03-07 17:15:44 -0800

    add wait to Eject_L4 named command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -233,6 +233,7 @@ public class RobotContainer {
             rollers.setTargetCommand(RollerState.EJECT_TOP),
             new WaitCommand(0.2),
             superstructure.goToStateCommand(SuperstructureState.INTAKE),
+            new WaitCommand(0.9),
             rollers.setTargetCommand(RollerState.INTAKE)));
 
     new EventTrigger("Score_L4")
