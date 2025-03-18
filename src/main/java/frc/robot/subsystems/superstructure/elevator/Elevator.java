681334f2e37e84d8c4f65041f36d7e081933a4d3
Nora <nizcat5@gmail.com>
2025-03-17 20:43:21 -0700

    Drive changes

--- src/main/java/frc/robot/subsystems/superstructure/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/superstructure/elevator/Elevator.java
@@ -7,7 +7,7 @@ import org.littletonrobotics.junction.Logger;
 public class Elevator extends GenericSuperstructure<Elevator.ElevatorTarget> {
   public enum ElevatorTarget implements GenericSuperstructure.PositionTarget {
     BOTTOM(0.6), // 25 and 7.25, made it a bit bigger
-    L1(11.8), // FIXME: 26 and 21.5
+    L1(10), // FIXME: 26 and 21.5
     L2(20.5), // 24 and 53.75
     L3(0), // 0 and 53.75
     SETUP_L4(31.6),
