4e9fff535b296152c4f1775df137dc4b95f2d6a4
Nora <nizcat5@gmail.com>
2025-04-02 17:43:29 -0700

    Make L3 not descore L4

--- src/main/java/frc/robot/subsystems/superstructure/pivot/Pivot.java
+++ src/main/java/frc/robot/subsystems/superstructure/pivot/Pivot.java
@@ -10,7 +10,7 @@ public class Pivot extends GenericSuperstructure<Pivot.PivotTarget> {
     L1(-110),
     L2(-107),
     SCORE_L3(138),
-    SETUP_L3(138),
+    SETUP_L3(100),
     CLIMB(140),
     ZERO_LOW(-95.2),
     ZERO_HIGH(90),
