fc9b61a65450dd5abf95d811bd56be6e9fdd9091
Bruce Peters <brucebenjaminpeters@gmail.com>
2025-06-30 22:35:56 -0700

    THE GREAT PURGE

--- src/main/java/frc/robot/subsystems/superstructure/elevator/ElevatorIO.java
+++
@@ -1,5 +0,0 @@
-package frc.robot.subsystems.superstructure.elevator;
-
-import frc.robot.subsystems.superstructure.GenericSuperstructureIO;
-
-public interface ElevatorIO extends GenericSuperstructureIO {}
--- src/main/java/frc/robot/subsystems/superstructure/pivot/PivotIO.java
+++
@@ -1,5 +0,0 @@
-package frc.robot.subsystems.superstructure.pivot;
-
-import frc.robot.subsystems.superstructure.GenericSuperstructureIO;
-
-public interface PivotIO extends GenericSuperstructureIO {}
